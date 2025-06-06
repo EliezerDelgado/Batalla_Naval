9521a55cf77f0e050a6077ee818b6656449ec2d3
eliezer <delgadoeliezer05@gmaill.com>
2025-06-06 21:45:51 +0200

    Spring v0.1

--- src/main/java/com/eliezer/Batalla_Naval/controller/PruebaContoller.java
+++ src/main/java/com/eliezer/Batalla_Naval/controller/PruebaContoller.java
@@ -10,4 +10,8 @@ public class PruebaContoller {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+    @GetMapping("/bye")
+    public String bye(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Bye bye %s! se yo tomorrow", name);
+    }
 }
