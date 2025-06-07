48c6d87bc1f98277eb0be94ee19de0766e8668c9
eliezer <delgadoeliezer05@gmaill.com>
2025-06-07 11:21:00 +0200

    Spring v0.1.1

--- src/main/java/com/eliezer/Batalla_Naval/controller/PruebaContoller.java
+++ src/main/java/com/eliezer/Batalla_Naval/controller/PruebaContoller.java
@@ -4,14 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
 @RestController
 public class PruebaContoller {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    ArrayList<String> list = new ArrayList<>();
+    int num = 1;
+    @GetMapping("/player")
+    public String player(@RequestParam(value = "name", defaultValue = "player "+1) String name) {
+        list.add(name);
         return String.format("Hello %s!", name);
     }
-    @GetMapping("/bye")
-    public String bye(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Bye bye %s! see you tomorrow", name);
+
+    @GetMapping("/list")
+    public String list() {
+        return String.join(System.lineSeparator(), list);
     }
 }
